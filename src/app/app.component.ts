ef3b7667ca775088a6053090a9a15195c9ce42d5
AnokPunx <arturflorian1@gmail.com>
2025-07-28 20:03:35 +0200

    bones no sanctuary

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {BrowserModule} from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet, RouterLink, RouterLinkActive], // Import HttpClientModule here],
   templateUrl: './app.component.html',
-  styleUrls: ['../demo-styling.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-quickstart';
+  title = 'untitled';
 }
