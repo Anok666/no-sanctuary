c9301b5b13f2f2aef81a6753a5e14592cc2febc9
AnokPunx <arturflorian1@gmail.com>
2025-07-29 14:32:42 +0200

    cypress test config ts

--- src/services/records.service.spec.ts
+++ src/services/records.service.spec.ts
@@ -1,16 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 
 import { RecordsService } from './records.service';
+import {HttpClient} from "@angular/common/http";
+import {HttpTestingController} from "@angular/common/http/testing";
 
 describe('RecordsService', () => {
   let service: RecordsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClient, HttpTestingController],
+      providers: [RecordsService]
+    });
     service = TestBed.inject(RecordsService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+  it('should be created',
+    () => {
+      expect(service).toBeTruthy();
+    });
 });
